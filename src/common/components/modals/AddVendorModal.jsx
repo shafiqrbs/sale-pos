5379471a41af8a6611f1aa9a41b12f5fd73e236d
sharif-minhaz <minhaz.rbs@gmail.com>
2026-03-04 10:55:55 +0600

    feat: integrate form validation and error handling in AddVendorModal; enhance user experience with validation wrappers for input fields

--- src/common/components/modals/AddVendorModal.jsx
+++ src/common/components/modals/AddVendorModal.jsx
@@ -5,19 +5,12 @@ import PhoneNumber from "@components/form-builders/PhoneNumberInput.jsx";
 import { useForm } from "@mantine/form";
 import { useAddVendorMutation } from "@services/core/vendors";
 import { showNotification } from "@components/ShowNotificationComponent.jsx";
+import FormValidationWrapper from "@components/form-builders/FormValidationWrapper.jsx";
 
 export default function AddVendorModal({ opened, onClose }) {
 	const { t } = useTranslation();
+	const [addVendor, { isLoading }] = useAddVendorMutation();
 
-	return (
-		<GlobalModal opened={opened} onClose={onClose} title={t("AddVendor")} size="sm">
-			<AddVendorModalForm onClose={onClose} />
-		</GlobalModal>
-	);
-}
-
-function AddVendorModalForm({ onClose }) {
-	const { t } = useTranslation();
 	const vendorForm = useForm({
 		initialValues: {
 			name: "",
@@ -34,12 +27,16 @@ function AddVendorModalForm({ onClose }) {
 				return null;
 			},
 			email: (value) => (value.trim() === "" ? "Email is required" : null),
+			company_name: (value) => (value.trim() === "" ? "Company name is required" : null),
 		},
 	});
-	const [addVendor, { isLoading }] = useAddVendorMutation();
 
 	const handleSubmit = async (values) => {
 		try {
+			const isFormValid = vendorForm.validate();
+
+			if (isFormValid.hasErrors) return;
+
 			const response = await addVendor({
 				name: values.name,
 				mobile: values.mobile,
@@ -61,60 +58,90 @@ function AddVendorModalForm({ onClose }) {
 	};
 
 	return (
-		<Box component="form" id="vendorForm">
-			<Grid gutter={6}>
-				<Grid.Col span={12}>
-					<TextInput
-						value={vendorForm.values.name}
-						onChange={(event) => vendorForm.setFieldValue("name", event.currentTarget.value)}
-						placeholder="Vendor Name"
-						size="xs"
-						name="name"
-					/>
-				</Grid.Col>
-				<Grid.Col span={12}>
-					<PhoneNumber form={vendorForm} name="mobile" placeholder="+880" size="xs" required />
-				</Grid.Col>
-				<Grid.Col span={12}>
-					<TextInput
-						value={vendorForm.values.email}
-						onChange={(event) => vendorForm.setFieldValue("email", event.currentTarget.value)}
-						placeholder="Email"
-						size="xs"
-						name="email"
-					/>
-				</Grid.Col>
-				<Grid.Col span={12}>
-					<TextInput
-						value={vendorForm.values.company_name}
-						onChange={(event) =>
-							vendorForm.setFieldValue("company_name", event.currentTarget.value)
-						}
-						placeholder="Company Name"
-						name="company_name"
-					/>
-				</Grid.Col>
-				<Grid.Col span={12}>
-					<Textarea
-						value={vendorForm.values.address}
-						onChange={(event) => vendorForm.setFieldValue("address", event.currentTarget.value)}
-						placeholder="Address"
-						name="address"
-						minRows={2}
-						maxRows={4}
-					/>
-				</Grid.Col>
-			</Grid>
-			<Flex justify="flex-end" mt="xs">
-				<Button
-					size="sm"
-					form="vendorForm"
-					onClick={vendorForm.onSubmit(handleSubmit)}
-					loading={isLoading}
-				>
-					{t("AddVendor")}
-				</Button>
-			</Flex>
-		</Box>
+		<GlobalModal opened={opened} onClose={onClose} title={t("AddVendor")} size="sm">
+			<Box component="form" id="vendorForm">
+				<Grid gutter={6}>
+					<Grid.Col span={12}>
+						<FormValidationWrapper
+							errorMessage={vendorForm.errors.name}
+							opened={!!vendorForm.errors.name}
+						>
+							<TextInput
+								value={vendorForm.values.name}
+								onChange={(event) => vendorForm.setFieldValue("name", event.currentTarget.value)}
+								placeholder="Vendor Name"
+								size="xs"
+								name="name"
+							/>
+						</FormValidationWrapper>
+					</Grid.Col>
+					<Grid.Col span={12}>
+						<PhoneNumber
+							form={vendorForm}
+							name="mobile"
+							placeholder="+880"
+							size="xs"
+							required
+							tooltip={vendorForm.errors.mobile}
+							opened={!!vendorForm.errors.mobile}
+						/>
+					</Grid.Col>
+					<Grid.Col span={12}>
+						<FormValidationWrapper
+							errorMessage={vendorForm.errors.email}
+							opened={!!vendorForm.errors.email}
+						>
+							<TextInput
+								value={vendorForm.values.email}
+								onChange={(event) => vendorForm.setFieldValue("email", event.currentTarget.value)}
+								placeholder="Email"
+								size="xs"
+								name="email"
+							/>
+						</FormValidationWrapper>
+					</Grid.Col>
+					<Grid.Col span={12}>
+						<FormValidationWrapper
+							errorMessage={vendorForm.errors.company_name}
+							opened={!!vendorForm.errors.company_name}
+						>
+							<TextInput
+								value={vendorForm.values.company_name}
+								onChange={(event) =>
+									vendorForm.setFieldValue("company_name", event.currentTarget.value)
+								}
+								placeholder="Company Name"
+								name="company_name"
+							/>
+						</FormValidationWrapper>
+					</Grid.Col>
+					<Grid.Col span={12}>
+						<FormValidationWrapper
+							errorMessage={vendorForm.errors.address}
+							opened={!!vendorForm.errors.address}
+						>
+							<Textarea
+								value={vendorForm.values.address}
+								onChange={(event) => vendorForm.setFieldValue("address", event.currentTarget.value)}
+								placeholder="Address"
+								name="address"
+								minRows={2}
+								maxRows={4}
+							/>
+						</FormValidationWrapper>
+					</Grid.Col>
+				</Grid>
+				<Flex justify="flex-end" mt="xs">
+					<Button
+						size="sm"
+						form="vendorForm"
+						onClick={vendorForm.onSubmit(handleSubmit)}
+						loading={isLoading}
+					>
+						{t("AddVendor")}
+					</Button>
+				</Flex>
+			</Box>
+		</GlobalModal>
 	);
 }
